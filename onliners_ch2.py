4e2dfa10170dc23cab4c3c0b1b0090b191636cc9
Julio <julio@ctrlsun.com>
2021-09-12 11:12:46 -0500

    Added ignores.

---
+++ onliners_ch2.py
@@ -0,0 +1,11 @@
+print([(x, y * 2) for x in range(3) for y in range(3)])
+print([x ** 2 for x in range(10) if x % 2 > 0])
+print([x.lower() for x in ["I", "AM", "NOT", "SHOUTING!"]])
+
+# Lambda functions
+txt = ['lambda functions are anonymous functions.',
+       'anonymous functions dont have a name.',
+       'functions are objects in Python.']
+
+mark = map(lambda s: (True, s) if "anonymous" in s else (False, s), txt)
+print(list(mark))
