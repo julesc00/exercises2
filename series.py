91280a2a426857b41730d69d08eb059d7c59cc41
Julio <julio@ctrlsun.com>
2021-09-13 09:18:26 -0500

    Added a series exercise.

---
+++ series.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+my_list = list("abcdefghijklmnopqrstuvwxyz")
+
+my_array = np.arange(26)
+
+my_dict = dict(zip(my_list, my_array))
+
+# Solution
+ser1 = pd.Series(my_list)
+ser2 = pd.Series(my_array)
+ser3 = pd.Series(my_dict)
+
+print(ser3.head())
