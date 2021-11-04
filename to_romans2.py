61c5be0fc4745b615bde3f9faa3db01cf2e03f78
Julio <julio@ctrlsun.com>
2021-11-04 13:11:44 -0600

    Better version of numbers to romans.

---
+++ to_romans2.py
@@ -0,0 +1,21 @@
+
+num_map = [
+    (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"),
+    (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+]
+
+
+def nums_to_roman(num):
+    roman = ""
+
+    while num > 0:
+        for i, r in num_map:
+            while num >= i:
+                roman += r
+                num -= i
+
+    return roman
+
+
+# Testing 1, 2, 3
+print(nums_to_roman(354))
