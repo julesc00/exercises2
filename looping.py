9e5a5832b9e176d74e5d54b29d02aad4e56f3502
Julio Briones <Julio_Briones@epam.com>
2021-11-16 12:16:27 -0600

    Added while loo  exercises.

--- looping.py
+++ looping.py
@@ -1,13 +1,17 @@
 
 num = 7
-user_input = input("Would like to play? (Y/n): ").lower()
 
-if user_input == "y":
-    user_num = int(input("Guess a number: "))
-    if user_num == num:
-        print("Got it!")
-    elif abs(num - user_num) == 1:
-        print("Off by one.")
+while True:
+    user_input = input("Would like to play? (Y/n): ").lower()
+    if user_input == "n":
+        print("Thanks :(")
+        break
     else:
-        print("Sorry, it's wrong")
-print("Thanks")
+        user_num = int(input("Guess a number: "))
+        if user_num == num:
+            print("Got it!")
+        elif abs(num - user_num) == 1:
+            print("Off by one.")
+        else:
+            print("Sorry, it's wrong")
+
