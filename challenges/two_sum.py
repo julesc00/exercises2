632ff02fa83040e16f8d2d4139e8ec043c644ed6
Julio Briones <Julio_Briones@epam.com>
2021-11-18 14:15:32 -0600

    Cleaning.

--- challenges/two_sum.py
+++ challenges/two_sum.py
@@ -1,21 +1,18 @@
 
 
 arr2 = [4, 7, 2, 6]
-target2 = 8
+target2 = 13
 
 
 def two_sum(lst: list[int], target: int) -> list[int]:
     values = {}
 
     for idx, elem in enumerate(lst):
-        print(f"index: {idx}, element: {elem}")
         comp = target - elem
-        print(f"comp: {comp}")
         if comp in values:
-            print(f"values: {values}")
             return [values[comp], idx]
-        print(f"other: {values}")
         values[elem] = idx
+
     return []
 
 
