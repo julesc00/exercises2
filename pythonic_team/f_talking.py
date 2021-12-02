237a05bff423d243bd9bcce93ad12ede96c4a35d
Julio Briones <Julio_Briones@epam.com>
2021-12-02 13:38:42 -0600

    Advanced in two normal consonant-vowel syllable translation, WIP.

--- pythonic_team/f_talking.py
+++ pythonic_team/f_talking.py
@@ -1,15 +1,29 @@
 
-def word_splitter_in_2(msg: str) -> str:
-    vowels = ["a", "e", "i", "o", "u"]
-    msg_split = [x for x in msg.split(" ")]
-    word_split = []
-    for word in msg_split:
-        for idx in range(0, len(word), 2):
-            word_split.append(word[idx:idx+2])
-    print(word_split)
-    print(msg_split)
-    msg_trans = []
+# def efe_lang_translator(msg: str):
+#     vowels = ["a", "e", "i", "o", "u"]
+#     msg_split = [x for x in msg.split(" ")]
+#     word_split = []
+#     trans_word = []
+#     trans_msg = []
+#     for word in msg_split:
+#         for idx in range(0, len(word), 2):
+#             word_trans = word[idx:idx+2]
+#             word_split.append(word_trans)
+#
+#     print(word_split)
+            # for syl in word_split:
+            #     if syl.endswith("a") or syl.endswith("e") or syl.endswith("i") or syl.endswith("o") or syl.endswith("u"):
+            #         trans_word = syl + f"f{syl[-1]}"
 
 
-word_splitter_in_2("Hola me llamo jose")
+# efe_lang_translator("Hola me como taco")
+
+
+def two_ltr_word_trans(word):
+    n = 2
+    word_split = [word[idx:idx+n] for idx in range(0, len(word), n)]
+    trans_word = [syl + f"f{syl[-1]}" for syl in word_split]
+    return trans_word
+
+print(two_ltr_word_trans("Hola"))
 
